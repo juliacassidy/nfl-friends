ecdb006ea22885747984d1827b5f7570d397a3b0
Julia Cassidy <juliacassidy@Julias-MacBook-Pro-2.local>
2019-10-05 22:41:06 -0400

    fixed pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,67 +1,67 @@
 var friendMatch = [
 
     {   name: "JuJu Smith-Schuster",
-        photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://usatsteelerswire.files.wordpress.com/2018/08/usatsi_11130474.jpg?w=1000&h=600&crop=1",
         scores: [5, 2, 3, 5, 5, 5, 5, 4, 4, 5],
     },
 
     {   name: "J.J. Watt",
-        photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://pbs.twimg.com/profile_images/871390705712680960/Ql0VzJzC_400x400.jpg",
         scores: [3, 4, 2, 4, 3, 5, 4, 5, 3, 3],
     },
 
     {   name: "Tom Brady",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://nesncom.files.wordpress.com/2013/11/brady-e1389912735753.jpg",
         scores: [1, 1, 1, 1, 3, 2, 1, 3, 4, 3],
     },
 
     {   name: "Aaron Rodgers",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://philadelphia.cbslocal.com/wp-content/uploads/sites/15116066/2013/01/159013540_10.jpg?w=800",
         scores: [5, 2, 2, 2, 3, 3, 5, 4, 2],
     },
 
     {   name: "Marshawn Lynch",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://i.imgflip.com/jryzw.jpg",
         scores: [1,3,4, 5, 1, 5, 4, 1, 2, 1],
     },
 
     {   name: "Ezekiel Elliott",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://sportshub.cbsistatic.com/i/r/2018/11/30/ca4fb983-8709-45e5-bcc1-003f9478f805/thumbnail/1200x675/4fdbf1a77d650e3eb127863fdb567c61/ezekiel-elliott-cowboys.jpg",
         scores: [5, 4, 4, 3, 4, 5, 5, 3, 4, 5],
     },
 
     {   name: "Le'Veon Bell",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://clutchpoints.com/wp-content/uploads/2019/03/Jets-news-Le_Veon-Bell_s-immediate-reaction-to-signing-with-New-York.jpg",
         scores: [5, 3, 5, 5, 5, 4, 3, 2, 2, 4],
     },
 
     {   name: "Cam Newton",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://i.kinja-img.com/gawker-media/image/upload/s--llfetGK9--/c_scale,f_auto,fl_progressive,q_80,w_800/1511624229074882958.jpg",
         scores: [3, 5, 5, 3, 4, 5, 3, 4, 1, 3],
     },
 
     {   name: "Odell Beckham Jr.",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://a.espncdn.com/photo/2019/0917/r599244_1296x518_5-2.jpg",
         scores: [5, 4, 5, 2, 5, 5, 5, 2, 3, 5],
     },
 
     {   name: "Drew Brees",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo:"http://media.al.com/press-register-sports/photo/drew-brees-for-soundoffjpg-9c03299bbe625db9.jpg",
         scores: [1, 4, 1, 5, 4, 5, 2, 5, 4, 3],
     },
 
     {   name: "Julio Jones",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://brobible.files.wordpress.com/2017/07/jliojones.jpg?quality=90&w=640",
         scores: [3, 2, 2, 2, 3, 4, 3, 4, 4, 2],
     },
 
     {   name: "Todd Gurley",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://usatramswire.files.wordpress.com/2017/12/gettyimages-894416330.jpg?w=1000&h=600&crop=1",
         scores: [2, 3, 2, 4, 4, 5, 3, 3, 5, 3],
     },
 
     {   name: "Patrick Mahomes II",
-    photo: "/Users/olympiacassidy/nfl-friends/gifs/juju.gif",
+        photo: "https://www.sportscasting.com/wp-content/uploads/2019/03/SC-Patrick-Mahomes-better-2019-2-GettyImages-1081592524.jpg",
         scores: [4, 3, 3, 3, 4, 5, 4, 5, 3, 3],
     },
 ]
