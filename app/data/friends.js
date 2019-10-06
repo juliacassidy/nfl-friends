2f5c40c2c4512df694b2892a781a71e0a5397de8
Julia Cassidy <juliacassidy@Julias-MacBook-Pro-2.local>
2019-10-05 21:43:12 -0400

    adding gifs

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+var friendMatch = [
+
+    {   name: "JuJu Smith-Schuster",
+        photo: "gifs/juju.gif",
+        scores: [5, 2, 3, 5, 5, 5, 5, 4, 4, 5],
+    },
+
+    {   name: "J.J. Watt",
+        photo: "gifs/watt.gif",
+        scores: [3, 4, 2, 4, 3, 5, 4, 5, 3, 3],
+    },
+
+    {   name: "Tom Brady",
+        photo: "gifs/TB.gif",
+        scores: [1, 1, 1, 1, 3, 2, 1, 3, 4, 3],
+    },
+
+    {   name: "Aaron Rodgers",
+        photo: "gifs/AR.gif",
+        scores: [5, 2, 2, 2, 3, 3, 5, 4, 2],
+    },
+
+    {   name: "Marshawn Lynch",
+        photo: "gifs/fine.gif",
+        scores: [1,3,4, 5, 1, 5, 4, 1, 2, 1],
+    },
+
+    {   name: "Ezekiel Elliott",
+        photo: "gifs/zeke.gif",
+        scores: [5, 4, 4, 3, 4, 5, 5, 3, 4, 5],
+    },
+
+    {   name: "Le'Veon Bell",
+        photo: "gifs/bell.gif",
+        scores: [5, 3, 5, 5, 5, 4, 3, 2, 2, 4],
+    },
+
+    {   name: "Cam Newton",
+        photo: "gifs/cam.gif",
+        scores: [3, 5, 5, 3, 4, 5, 3, 4, 1, 3],
+    },
+
+    {   name: "Odell Beckham Jr.",
+        photo: "gifs/obj.gif",
+        scores: [5, 4, 5, 2, 5, 5, 5, 2, 3, 5],
+    },
+
+    {   name: "Drew Brees",
+        photo: "/gifs/brees.gif",
+        scores: [1, 4, 1, 5, 4, 5, 2, 5, 4, 3],
+    },
+
+    {   name: "Julio Jones",
+        photo: "gifs/julio.gif",
+        scores: [3, 2, 2, 2, 3, 4, 3, 4, 4, 2],
+    },
+
+    {   name: "Todd Gurley",
+        photo: "gifs/gurley.gif",
+        scores: [2, 3, 2, 4, 4, 5, 3, 3, 5, 3],
+    },
+
+    {   name: "Patrick Mahomes II",
+        photo: "gifs/mahomes.gif",
+        scores: [4, 3, 3, 3, 4, 5, 4, 5, 3, 3],
+    },
+]
+  
+  
+  module.exports = friendMatch;
