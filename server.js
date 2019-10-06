b1bb7296e14dcbc49c75411d2ac421a217b19ba4
Julia Cassidy <juliacassidy@Julias-MacBook-Pro-2.local>
2019-10-05 22:10:28 -0400

    functionality

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = process.env.PORT || 7000;
+var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
